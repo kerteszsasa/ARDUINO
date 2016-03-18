c4bb07f715cc5b7d1144abe760280a4e9fba8161
kerteszsasa <kerteszsasa@users.noreply.github.com>
2016-03-18 18:08:14 +0100

    SURF init commit

---
+++ SURF_controller/SURF_controller.ino
@@ -0,0 +1,42 @@
+#define RED                           10
+#define GREEN                         9
+#define BLUE                          5
+#define MODE_SELECT_PIN               A6
+int MODE_RECEIEVER = 0;
+int MODE_TRANSMITTER = 0;
+
+
+ 
+
+
+// the setup routine runs once when you press reset:
+void setup() {                
+  //RGB init
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+  digitalWrite(RED, HIGH);
+  digitalWrite(GREEN, HIGH);
+  digitalWrite(BLUE, HIGH);    
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+
+  delay(1000);               // wait for a second
+
+  if(analogRead(A6) > 512){
+    digitalWrite(RED, LOW);
+  }
+  else{
+    digitalWrite(BLUE, LOW);
+  }
+
+delay(1000); 
+  digitalWrite(RED, HIGH);
+  digitalWrite(BLUE, HIGH);
+
+
+
+  
+}
